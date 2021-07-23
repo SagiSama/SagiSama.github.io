ad9989c5a3c8f9c53673b586616242d07f5f9a7f
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-23 18:17:59 -0500

    f

--- javascript/javascript_functions_l1.js
+++ javascript/javascript_functions_l1.js
@@ -7,4 +7,13 @@ function max(num1, num2) {
 	}
 }
 
+// Task test
+function test(expected, result) {
+	if(expected === result) {
+		console.log("Test success");
+	} else {
+		console.log("Test failed");
+	}
+}
+
 console.log("test of one ---->"+max(1,2));
