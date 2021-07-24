2be0b186f76e1655326c35e0a39510830e7b726f
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-23 19:50:26 -0500

    Update javascript_functions_l1.js

--- javascript/javascript_functions_l1.js
+++ javascript/javascript_functions_l1.js
@@ -18,6 +18,24 @@ const isVowel = function(vovel) {
 	return charString.indexOf(vovel) > -1;
 };
 
+//task4
+const sum = function sum(arr) {
+    let sum=0;
+	for(let i=0; i<arr.length; i++) {
+	  sum = sum + arr[i];
+	}
+	return sum;
+};
+
+const multiply = multiply1(arr) {
+	let total=0, i=0;
+	while(i<arr.length) {
+		total += arr[i];
+		i++;
+	}
+	return total;
+};
+
 // Task test
 function test(expected, result) {
 	if(expected === result) {
@@ -29,4 +47,6 @@ function test(expected, result) {
 
 console.log("Task 1 -> Expected output of max(1, -2) is 1  " + test(1, max(1, -2)));
 console.log("Task 2 -> Expected output of maxOfThree(1, -2, 0) is 1  " + test(1, maxOfThree(1, -2, 0)));
-console.log("Task 4 -> Expected output of isVowel('a') is true  " + test(true, isVowel("a")));
+console.log("Task 3 -> Expected output of isVowel('a') is true  " + test(true, isVowel("a")));
+console.log("Task 4 -> Expected output of sum([1,2,3,4,5,6,7,8,9]) is 1  " + test(1, sum([1,2,3,4,5,6,7,8,9])));
+console.log("Task 4 -> Expected output of multiply([1,2,3,4,5,6,7,8,9]) is 1  " + test(1, multiply([1,2,3,4,5,6,7,8,9])));
