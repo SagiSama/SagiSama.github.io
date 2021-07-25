8248497278f0ebcd94b058af72215234710e9363
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-25 16:48:43 -0500

    Update ascii.js

--- javascript/ascii.js
+++ javascript/ascii.js
@@ -1,3 +1,11 @@
-function test() {
+window.onload = function(){
+    "use strict";
+    // put all of your code here
+	
+	let buttonStart = document.getElementById("start");
+	buttonStart.onevent = start;
+}
+
+function start() {
 	alert("hi");
 }
