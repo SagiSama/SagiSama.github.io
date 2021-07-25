ad00438af6bbec937e04e25b455312b054d39644
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-25 17:37:19 -0500

    c

--- javascript/ascii.js
+++ javascript/ascii.js
@@ -1,9 +1,17 @@
 window.onload = function() {
 	
+	var mainTextArea = document.getElementById("text-area");
 	var buttonStart = document.getElementById("start");
 	buttonStart.onclick = start;
+	var selectAnimation = document.getElementById("animation");
+	selectAnimation.onchange = displayAnimationText;
 
 	function start() {
-		alert("hi");
+		alert("start animation !!!");
+	}
+	
+	function displayAnimationText() {
+		alert(selectAnimation.value);
+		mainTextArea.innerText = selectAnimatio.value;
 	}
 };
