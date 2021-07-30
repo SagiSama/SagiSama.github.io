cdda32f5baff78508b29a66f914e32f5c2c1a847
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-29 22:30:21 -0500

    f

--- javascript/inheritance.js
+++ javascript/inheritance.js
@@ -22,3 +22,25 @@ Array.prototype.bubbleSort = function() {
   }
   return this;
 }
+
+// Exersice 3
+
+var Person = function() {};
+Person.prototype.initialize = function(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person.prototype.describe = function() {
+  return this.name + ", " + this.age + " years old.";
+}
+
+var Teacher = function() {};
+  Teacher.prototype = new Person();
+  Teacher.prototype.teach = function(subject) {
+  return this.name + " is now teaching " + subject;
+}
+var me = new Teacher();
+
+me.initialize("John", 25);
+var result = me.teach("Inheritance");
+console.log(result);
--- javascript/inheritance_tests.js
+++ javascript/inheritance_tests.js
@@ -5,9 +5,17 @@ describe("filter", function () {
         });
 });
 
-describe("filter1", function () {
-    it("Filter returns filtered string by banned string list",
+describe("bubbleSort", function () {
+    it("bubbleSort returns sorted array",
         function () {
         	assert.equal([-9,-1,0,1,2,3,3,4].toString(), [1,2,3,4,0,-1,-9,3].bubbleSort().toString());
+        });
+});
+
+describe("teach", function () {
+    it("teach returns Teacher object inherited from Person",
+        function () {
+        	me.initialize("John", 25);
+        	assert.equal("John is now teaching Inheritance", me.teach("Inheritance"));
         });
 });
