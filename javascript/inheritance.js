fcbd797686c6e397b9d8bf26a70137115ee839a7
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-29 22:35:30 -0500

    Update inheritance.js

--- javascript/inheritance.js
+++ javascript/inheritance.js
@@ -44,3 +44,49 @@ var me = new Teacher();
 me.initialize("John", 25);
 var result = me.teach("Inheritance");
 console.log(result);
+
+// OLD APPROACHES
+
+// Exersice 1
+/*
+function String(...value) {
+    this.value = value;
+}
+String.prototype.filter = function(bannedList) {
+    var strArr = this.value.toString().split(' ');
+    return strArr
+        .filter((str) => { return !bannedList.includes(str) })
+        .join(' ');
+}
+var string = new String("IT is not good!");
+var bannedList = ['is', 'not'];
+console.log("=-> "+string.filter(bannedList));
+
+// Approach 2
+String.prototype.filter1 = function(bannedList) {
+ return this.split(" ").filter((str) => { return !bannedList.includes(str) })
+        .join(' ');;
+};
+
+// Exersice 2
+
+function Arr(value) {
+  this.value = value;
+}
+Arr.prototype.bubbleSort = function() {
+  var result = [];
+  for(var i=0; i<this.value.length; i++) {
+    for(var j=0; j<this.value.length; j++) {
+      var tmp;
+      if(this.value[i]<this.value[j]) {
+        tmp = this.value[i];
+        this.value[i] = this.value[j];
+        this.value[j] = tmp;
+      }
+    }
+  }
+  return this.value;
+}
+var arr = new Arr([1,2,3,4,0,-1,-9,3]);
+console.log("=-> "+arr.bubbleSort());
+*/
