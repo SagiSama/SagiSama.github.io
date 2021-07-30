3639eca20fa86e32c422f8731a931716961fe473
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-29 22:17:46 -0500

    ff

--- javascript/inheritance.js
+++ javascript/inheritance.js
@@ -1,53 +1,24 @@
 // Exersice 1
 
-function String(...value) {
-    this.value = value;
-}
 String.prototype.filter = function(bannedList) {
-    var strArr = this.value.toString().split(' ');
-    return strArr
-        .filter((str) => { return !bannedList.includes(str) })
-        .join(' ');
-}
-var string = new String("IT is not good!");
-var bannedList = ['is', 'not'];
-// console.log("=-> "+string.filter(bannedList));
-
-// Approach 2
-String.prototype.filter1 = function(bannedList) {
  return this.split(" ").filter((str) => { return !bannedList.includes(str) })
         .join(' ');;
 };
 
 // Exersice 2
 
-function Arr(value) {
-  this.value = value;
-}
-Arr.prototype.bubbleSort = function() {
+Array.prototype.bubbleSort = function() {
   var result = [];
-  for(var i=0; i<this.value.length; i++) {
-    for(var j=0; j<this.value.length; j++) {
+  console.log(this.length)
+  for(var i=0; i<this.length; i++) {
+    for(var j=0; j<this.length; j++) {
       var tmp;
-      if(this.value[i]<this.value[j]) {
-        tmp = this.value[i];
-        this.value[i] = this.value[j];
-        this.value[j] = tmp;
+      if(this[i]<this[j]) {
+        tmp = this[i];
+        this[i] = this[j];
+        this[j] = tmp;
       }
     }
   }
-  return this.value;
-}
-var arr = new Arr([1,2,3,4,0,-1,-9,3]);
-// console.log("=-> "+arr.bubbleSort());
-
-// Exersice 3
-
-var Person = function() {};
-Person.prototype.initialize = function(name, age) {
-    this.name = name;
-    this.age = age;
-}
-Person.prototype.describe = function() {
-    return this.name + ", " + this.age + " years old.";
+  return this;
 }
--- javascript/inheritance_tests.js
+++ javascript/inheritance_tests.js
@@ -1,6 +1,13 @@
+describe("filter", function () {
+    it("Filter returns filtered string by banned string list",
+        function () {
+        	assert.equal("IT good!", "IT is not good!".filter(['is', 'not']));
+        });
+});
+
 describe("filter1", function () {
     it("Filter returns filtered string by banned string list",
         function () {
-        	assert.equal("IT good!", "IT is not good!".filter1(['is', 'not']));
+        	assert.equal([-9,-1,0,1,2,3,3,4], [1,2,3,4,0,-1,-9,3].bubbleSort());
         });
 });
