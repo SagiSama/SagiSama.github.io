6d0b3e268b47c07abd153adc15889ed030af71f8
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-07-29 22:13:53 -0500

    Update inheritance_tests.js

--- javascript/inheritance_tests.js
+++ javascript/inheritance_tests.js
@@ -1,6 +1,6 @@
 describe("filter1", function () {
     it("Filter returns filtered string by banned string list",
         function () {
-            assert.equal("IT good!", "IT is not good!".filter1(['is', 'not']));
+        	assert.equal("IT good!", "IT is not good!".filter1(['is', 'not']));
         });
 });
