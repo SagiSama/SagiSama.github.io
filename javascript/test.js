d0ff9aa7a84e347368895a9d33b8f7448a18f5a1
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-08-02 22:11:59 -0500

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -4,3 +4,17 @@ describe("getNumber", function () {
             assert.equal(12, new Account(12).getNumber());
         });
 });
+
+describe("getBalance", function () {
+    it("getBalance returns account's balance",
+        function () {
+            assert.equal(0.0, new Account(12).getBalance());
+        });
+});
+
+describe("deposit", function () {
+    it("deposit returns account's balance add to deposit",
+        function () {
+            assert.equal(14, new Account(12).deposit(2));
+        });
+});
