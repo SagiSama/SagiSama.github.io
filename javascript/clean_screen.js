8095f409b495f3a446e1de96349d62f5f75cde9d
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-08-03 20:22:17 -0500

    1

--- javascript/clean_screen.js
+++ javascript/clean_screen.js
@@ -5,13 +5,6 @@ $(function() {
   	var rand = back[Math.floor(Math.random() * back.length)];
   	$('div').css('background', rand);
   	
-  	//$('div').click(function(e) {
-    //	$('div').remove('div');
-	//});
-	
-	//$('div').click(function(e){
-	//    $(e.target).remove();
-	//});
 	$('div').click(function() { 
 		$(this).remove(); 
 	});
@@ -21,7 +14,6 @@ let size = 50;
 //var timer = setInterval(function() { grow(); }, 250);
 
 var grow = function() {
-	console.log("called ===");
 	size += 10;
 	$("div").width(size).height(size).removeClass('ball').addClass('ball');
 	$('div').css('border-radius', size+'px');
