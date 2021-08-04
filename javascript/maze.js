9b0c890b539f154a3fd1fd66ba9d8ded7546bbde
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-08-04 18:02:02 -0500

    Update maze.js

--- javascript/maze.js
+++ javascript/maze.js
@@ -1,19 +1,18 @@
 $(function(){
 	let status = false;
 	let collision = 0;
-	let state = 'Click the "S" to begin.';
 	
 	$( "#start" ).click(function() {
 		status = true;
 		collision = 0;
-		$('#status').text('Game started! '+state+$('#status').text());
+		$('#status').text('Game started! '+$('#status').text());
 	});
 	
 	$("#end").mouseenter(function(){
 		if(status && collision <= 0) {
-			$('#status').text('Congrats, won! :) ' + state);
+			$('#status').text('Congrats, won! :) ' + $('#status').text());
 		} else if(status && collision > 0) {
-			$('#status').text('Lost! ;( ' + state);
+			$('#status').text('Lost! ;( ' + $('#status').text());
 		}
 		status = false;
 	});
