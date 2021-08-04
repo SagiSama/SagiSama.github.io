d6f1f0736582204e041a388a5260d8175148a66e
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-08-03 21:58:47 -0500

    Update clean_screen.js

--- javascript/clean_screen.js
+++ javascript/clean_screen.js
@@ -32,8 +32,8 @@ var addBall = function() {
 	 	"css": {
 	 		"background-color": color,
 	 		"position": "absolute",
-	 		"left": generateRandomPosition+'%',
-	 		"top": generateRandomPosition+'%',
+	 		"left": generateRandomPosition()+'%',
+	 		"top": generateRandomPosition()+'%',
 	 		'border-radius': parseInt($('#width').val())+'px',
 	 	}
 	}));
