320fb2a436a134a4f37e9dbeeddfa6b8129a6877
SagiSama <62695992+SagiSama@users.noreply.github.com>
2021-08-03 19:09:01 -0500

    Update clean_screen.js

--- javascript/clean_screen.js
+++ javascript/clean_screen.js
@@ -8,6 +8,6 @@ var timer = setInterval(function() { grow(); }, 250);
 var grow = function() {
 	console.log("called ===");
 	size += 10;
-	$("div").width(size).height(size).addClass('ball');
+	$("div").width(size).height(size).removeClass('ball').addClass('ball');
 	$('div').css('border-radius', size+'px');
 }
